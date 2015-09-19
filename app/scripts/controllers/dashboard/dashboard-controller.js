0527480b2a71777db81d9a4618e15d56c2c6974e
Fabricio Flores <fabrifloresg@gmail.com>
2015-09-19 14:25:11 -0500

    List neurons

--- app/scripts/controllers/dashboard/dashboard-controller.js
+++ app/scripts/controllers/dashboard/dashboard-controller.js
@@ -3,10 +3,11 @@
   angular.module('starter.controllers', ['config'])
   .controller('DashCtrl', function($scope, $http, ENV) {
     var loadData = function(){
-      $http.get(ENV.apiEndpoint + '/admin/neurons.json')
-        .then(function(r){
-          console.log(r);
-          // we need work in backend. stand by
+      $http.get(ENV.apiEndpoint + 'admin/neurons.json')
+        .then(function(response){
+          $scope.neurons = response.data.neurons;
+        }, function(error){
+          console.log(error.message);
         });
     };
 
