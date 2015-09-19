c5d9b0b9c9926e61c173952928e7d01a7b4ba03e
Fabricio Flores <fabrifloresg@gmail.com>
2015-09-19 11:00:57 -0500

    Add some logic to get neurons

--- app/scripts/controllers/dashboard/dashboard-controller.js
+++ app/scripts/controllers/dashboard/dashboard-controller.js
@@ -1,7 +1,15 @@
 (function(){
   'use strict';
-  angular.module('starter.controllers')
-  .controller('DashCtrl', function($scope) {
-    console.log($scope);
+  angular.module('starter.controllers', ['config'])
+  .controller('DashCtrl', function($scope, $http, ENV) {
+    var loadData = function(){
+      $http.get(ENV.apiEndpoint + '/admin/neurons.json')
+        .then(function(r){
+          console.log(r);
+          // we need work in backend. stand by
+        });
+    };
+
+    loadData();
   });
 })();
