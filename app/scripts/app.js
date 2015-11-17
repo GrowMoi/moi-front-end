c0d762dc58015d20c68d05c57c3cf56d3ae3b7af
Mario Andrés Correa <a@macool.me>
2015-11-16 23:43:32 -0500

    bootstrapping login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,6 +33,12 @@
     // Each state's controller can be found in controllers.js
     $stateProvider
 
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+    })
+
     // setup an abstract state for the tabs directive
     .state('tab', {
       url: '/tab',
---
+++ app/scripts/services/login-service.js
@@ -0,0 +1,28 @@
+(function(){
+  'use strict';
+
+  angular.module('starter.services')
+  .factory('LoginService', function($q) {
+    return {
+      loginUser: function(name, pw) {
+        var deferred = $q.defer();
+        var promise = deferred.promise;
+
+        if (name == 'user' && pw == 'secret') {
+          deferred.resolve('Welcome ' + name + '!');
+        } else {
+          deferred.reject('Wrong credentials.');
+        }
+        promise.success = function(fn) {
+          promise.then(fn);
+          return promise;
+        }
+        promise.error = function(fn) {
+          promise.then(null, fn);
+          return promise;
+        }
+        return promise;
+      }
+    };
+  });
+})();
