9ffe1e1d3456a58fa3f616ac84113446b272f33b
Fabricio Flores <fabrifloresg@gmail.com>
2015-11-19 17:43:41 -0500

    Move from tabs to sidebar

--- app/scripts/controllers/login/login-controller.js
+++ app/scripts/controllers/login/login-controller.js
@@ -6,13 +6,13 @@
     $scope.loginForm = {};
 
     $rootScope.$on('auth:validation-success', function () {
-      $state.go('tab.dash');
+      $state.go('menu.dash');
     });
 
     $scope.login = function() {
       $auth.submitLogin($scope.loginForm)
         .then(function () {
-          $state.go('tab.dash');
+          $state.go('menu.dash');
         })
         .catch(function (resp) {
           $ionicPopup.alert({
