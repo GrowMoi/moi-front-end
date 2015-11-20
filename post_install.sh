4e73194e4c9ff5fa917edefdb7ad1c815b82ec0f
Mario Andrés Correa <a@macool.me>
2015-11-19 22:26:25 -0500

    build env's gruntfile only if available

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 ./node_modules/bower/bin/bower install
-./node_modules/grunt-cli/bin/grunt --gruntfile Gruntfile.$ENVIRONMENT.js build
+
+# build if there's a Gruntfile for
+# this environment. Currently used
+# to compress on staging deployment
+GRUNTFILE="Gruntfile.$ENVIRONMENT.js"
+if [ -e "$GRUNTFILE" ]
+then
+  ./node_modules/grunt-cli/bin/grunt --gruntfile $GRUNTFILE build
+fi
+
+exit $?
