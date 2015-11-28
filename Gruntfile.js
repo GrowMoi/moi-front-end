796bc12a7918fdea43f20e03ca85f181bd42f7c9
Mario Andrés <a@macool.me>
2015-11-28 12:58:50 -0500

    require task not required

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,8 +14,6 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
-  grunt.loadNpmTasks('grunt-protractor-runner');
-
   // Define the configuration for all the tasks
   grunt.initConfig({
 
