4177a0e5e92341c18f1625363047031a4553073f
Mario Andrés <a@macool.me>
2016-01-15 12:08:31 -0500

    Update site-controller.js

--- app/scripts/controllers/site/site-controller.js
+++ app/scripts/controllers/site/site-controller.js
@@ -8,9 +8,6 @@
   function SiteController($rootScope,
                           $ionicLoading) {
 
-    // model... let's comment until we need it
-    // var site = this;
-
     //This must be the only place where we need to listen stateChanges
     $rootScope.$on('$stateChangeStart', function(){
       $ionicLoading.show({
