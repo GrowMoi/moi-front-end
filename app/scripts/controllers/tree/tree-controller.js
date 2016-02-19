9c07d975853c4d2f4180bcfc652343bf14743405
Darwin Gualotuña <daosgc@gmail.com>
2016-02-19 10:58:27 -0500

    add intro music on tree-page

--- app/scripts/controllers/tree/tree-controller.js
+++ app/scripts/controllers/tree/tree-controller.js
@@ -2,13 +2,22 @@
   'use strict';
 
   angular.module('moi.controllers')
-  .controller('TreeController', function ($state) {
+  .controller('TreeController', function ($state, $document) {
     var vm = this;
 
     vm.transitionToContent = function () {
       console.log('transitionToContent');
       $state.go('neuron');
     };
+
+    var vid = $document[0].getElementById('music-moi');
+
+    function playVid() {
+        vid.play();
+    }
+
+    playVid();
+
   });
 
 })();
