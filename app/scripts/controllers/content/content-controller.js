362b66890b1bdc4946a25fbce4fea467b1702736
Darwin Gualotuña <daosgc@gmail.com>
2016-03-01 10:44:16 -0500

    add modal template and add nota in template

--- app/scripts/controllers/content/content-controller.js
+++ app/scripts/controllers/content/content-controller.js
@@ -1,10 +1,31 @@
 (function(){
   'use strict';
   angular.module('moi.controllers')
-  .controller('ContentController', function(content) {
+  .controller('ContentController', function($ionicModal, $scope, content, ContentService) {
     var vm = this;
 
     vm.content = content;
+    vm.sendNotes = sendNotes;
 
+    $ionicModal.fromTemplateUrl('templates/partials/modal-image.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      vm.modal = modal;
+    });
+
+    $scope.imageSrc = 'images/lluvia_meteoros.jpg';
+
+    vm.showImage = function() {
+      vm.modal.show();
+    };
+
+    vm.closeModal = function() {
+      vm.modal.hide();
+    };
+
+    function sendNotes() {
+      ContentService.addNotesToContent(vm.content);
+    }
   });
 })();
