7f6ce0ca0eda3745f084e05f5bd570205a773251
Darwin Gualotuña <daosgc@gmail.com>
2016-06-09 17:13:22 -0500

    add infinite-scroll in search page

--- app/scripts/controllers/searches/searches-controller.js
+++ app/scripts/controllers/searches/searches-controller.js
@@ -1,17 +1,46 @@
 (function(){
   'use strict';
   angular.module('moi.controllers')
-  .controller('SearchesController', function(NeuronService)
+  .controller('SearchesController', function($scope, $ionicLoading, NeuronService)
   {
     var searchesmodel = this;
     searchesmodel.query = '';
     searchesmodel.search = search;
+    searchesmodel.currentPage = 1;
 
     function search() {
-      NeuronService.searchNeurons(searchesmodel.query, 1).then(function(resp) {
+      // Setup the loader
+       $ionicLoading.show({
+         content: 'Loading',
+         animation: 'fade-in',
+         showBackdrop: true,
+         showDelay: 0
+       });
+      NeuronService.searchNeurons(searchesmodel.query, searchesmodel.currentPage).then(function(resp) {
+        searchesmodel.currentPage += 1;
         searchesmodel.neurons = resp.search;
-        console.log('neurons', searchesmodel.neurons);
+        /*jshint camelcase: false */
+        searchesmodel.totalItems = resp.meta.total_items;
+      }).finally(function(){
+        $ionicLoading.hide();
       });
     }
+
+    searchesmodel.noMoreItemsAvailable = false;
+
+    searchesmodel.loadMore = function() {
+      NeuronService.searchNeurons(searchesmodel.query, searchesmodel.currentPage).then(function(resp) {
+        searchesmodel.neurons = searchesmodel.neurons.concat(resp.search);
+        /*jshint camelcase: false */
+        searchesmodel.totalItems = resp.meta.total_items;
+        searchesmodel.currentPage += 1;
+        if ( searchesmodel.neurons.length === searchesmodel.totalItems ) {
+          searchesmodel.noMoreItemsAvailable = true;
+        }
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+    };
+
+    searchesmodel.neurons = [];
   });
 })();
