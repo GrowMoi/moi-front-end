eca4eaf52daa4c07cfa56cbc127cfb706171efd4
Jorge Mejia <jmejia@ioet.com>
2017-03-09 03:03:51 -0500

    fix directive title (#96)

--- app/scripts/directives/responsive-text/responsive-text.js
+++ app/scripts/directives/responsive-text/responsive-text.js
@@ -8,28 +8,32 @@
         var directive = {
           restrict: 'EA',
           scope: {
-            height: '='
+            height: '=',
+            title: '='
           },
           link: function(scope, element) {
-                    resize(scope, element);
+                  element[0].textContent = scope.title;
+                  resize(scope, element);
                 }
         };
         function resize(scope, element) {
-          $timeout(function(){
-            var heightDivInit = scope.height;
-            var divVal = $window.getComputedStyle(element[0], null);
-            var fontSize = parseInt(divVal.fontSize);
-            var fontSizeCalc = fontSize;
-            var heightDiv = parseInt(divVal.height);
-            var heightDivCalc = heightDiv;
+          var heightDivInit = scope.height;
+          var divVal = $window.getComputedStyle(element[0], null);
+          var fontSize = parseInt(divVal.fontSize);
+          var fontSizeCalc = fontSize;
+          var heightDiv = parseInt(divVal.height);
+          var heightDivCalc = heightDiv;
 
-            while (heightDivCalc >= heightDivInit) {
-              fontSizeCalc = fontSizeCalc - 1;
-              heightDivCalc = (fontSizeCalc * heightDiv)/fontSize;
-            }
+          while (heightDivCalc < heightDivInit) {
+            fontSizeCalc = fontSizeCalc + 1;
             element.css('fontSize', fontSizeCalc+'px');
-            element.css('height', heightDivInit+'px');
-          });
+            heightDivCalc = calculate(element, heightDivCalc);
+          }
+          element.css('height', heightDivInit+'px');
+        }
+        function calculate(element, heightDivCalc){
+          heightDivCalc = element[0].offsetHeight;
+          return heightDivCalc;
         }
         return directive;
       }
