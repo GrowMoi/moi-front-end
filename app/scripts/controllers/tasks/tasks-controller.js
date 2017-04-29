2d3648909e94e8842e878aeb401cbaab61d34abf
Darwin Gualotuña <daosgc@gmail.com>
2017-04-29 18:28:31 -0500

    add tabs into task page

--- app/scripts/controllers/tasks/tasks-controller.js
+++ app/scripts/controllers/tasks/tasks-controller.js
@@ -2,6 +2,39 @@
   'use strict';
   angular.module('moi.controllers')
   .controller('TasksController', function(){
-    // var tasksmodel = this;
+    var tasksmodel = this;
+
+    tasksmodel.tabs = [
+      {
+        field:'notes',
+        name: 'Notas',
+        image: 'images/notes_tasks.png',
+        selected: false
+      },
+      {
+        field:'recomendations',
+        name: 'Recomendaciones',
+        image: 'images/recomendations_tasks.png',
+        selected: false
+      },
+      {
+        field:'tasks',
+        name: 'Tareas',
+        image: 'images/notifications_tasks.png',
+        selected: true
+      },
+      {
+        field:'notifications',
+        name: 'Notificaciones',
+        image: 'images/notifications_tasks.png',
+        selected: false
+      },
+      {
+        field:'favorites',
+        name: 'Favoritos',
+        image: 'images/favorites_tasks.png',
+        selected: false
+      }
+    ];
   });
 })();
