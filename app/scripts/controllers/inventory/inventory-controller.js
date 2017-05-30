730047ecda55893bfd111a0bb83153f147f44667
Lenin Capa <leviskp87@gmail.com>
2017-05-29 23:00:59 -0500

    add btns

--- app/scripts/controllers/inventory/inventory-controller.js
+++ app/scripts/controllers/inventory/inventory-controller.js
@@ -3,6 +3,18 @@
   angular.module('moi.controllers')
     .controller('InventoryController', function() {
       var vm = this;
-      vm.test = 'test';
+
+      vm.buttonsOptions = {
+        neuron: null,
+        content: null,
+        buttons: {
+          learn: true,
+          search: true,
+          share: true,
+          recomendation: true,
+          saveTask: true,
+          showTasks: true
+        }
+      };
     });
 })();
