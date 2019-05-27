4a827d23e4e810359e027ccdc7a7b862d99abd7f
Darwin Gualotuña <daosgc@gmail.com>
2019-05-27 15:44:43 -0500

    add improvements into leadeboard

--- app/scripts/services/leader-board-service.js
+++ app/scripts/services/leader-board-service.js
@@ -18,7 +18,8 @@
           dialogOptions = {
             templateUrl: 'templates/partials/modal-show-leaderboard.html',
             model: {
-              goToUser: goToUser
+              goToUser: goToUser,
+              close: closeLeadeboardModal
             }
           };
 
@@ -29,18 +30,19 @@
       return service;
 
       function showLeaderboard(entity, fromEvent){
+        entity = entity;
         UserService.getLeaderboard(entity, currentPage, itemsPerPage).then(function(data) {
             dialogOptions.model.leaders = data.leaders;
             dialogOptions.model.user = data.meta.user_data; //jshint ignore:line
             dialogOptions.model.total_contents = data.meta.total_contents; //jshint ignore:line
+            dialogOptions.model.total_super_event_achievements = data.meta.total_super_event_achievements; //jshint ignore:line
             dialogOptions.model.hideFooter = isCurrentUserLeader(data.leaders);
-            dialogOptions.model.noMoreItemsAvailable = false;
             dialogOptions.model.nextPage = loadMoreItems;
             dialogOptions.model.fromEvent = fromEvent;
+            total_pages =  data.meta.total_pages; //jshint ignore:line
+            dialogOptions.model.noMoreItemsAvailable = currentPage === total_pages;//jshint ignore:line
             ModalService.showModel(dialogOptions);
             currentPage += 1;
-            entity = entity;
-            total_pages =  data.meta.total_pages; //jshint ignore:line
         });
       }
 
@@ -59,16 +61,29 @@
         });
       }
 
+      function resetPagination() {
+        currentPage = 1;
+        itemsPerPage = 10;
+        total_pages = 0; //jshint ignore:line
+        isGettingItems = false;
+        entity = {};
+      }
+
       function isCurrentUserLeader(leaders){
         var leader = leaders.find(function(leader){return leader.user_id === $auth.user.id;}); //jshint ignore:line
         return leader ? true : false;
       }
 
       function goToUser(user){
-        dialogOptions.model.closeModal();
+        closeLeadeboardModal();
         $state.go('profile', {
           username: user.username
         });
+      }
+
+      function closeLeadeboardModal(){
+        dialogOptions.model.closeModal();
+        resetPagination();
       }
     }
   })();
