88c1507e4ce9134cc8d3997d81ae75473d96e6a3
Mario Andrés Correa <a@macool.me>
2019-09-30 10:51:40 -0500

    set stage

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,7 @@ set :slackistrano, {
 
 set :deploy_to, '/home/growmoi/moi-front-end'
 set :nvm_node, 'v0.12.14'
+set :stage, 'production'
 set :release_stage_name, 'production'
 
 # Default value for :format is :airbrussh.
