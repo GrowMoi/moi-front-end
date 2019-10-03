3ccc39855577998c97ff3b5191b630e8c81a2ec6
Darwin Gualotuña <daosgc@gmail.com>
2019-10-03 12:53:30 -0500

    Update sounds app and remove scrollbar into neuron page

--- app/scripts/values/sounds-page.js
+++ app/scripts/values/sounds-page.js
@@ -13,69 +13,69 @@
         type: 'wav'
       },
       neuron: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       content: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       settings: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       test: {
-        sound: 'sounds/test.mp3',
+        sound: 'sounds/test_sound.m4a',
         type: 'mp3'
       },
       tree: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3'
       },
       searches: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       profile: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       profileEdit: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       friends: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       tasks: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       inventory:{
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       guide: {
-        sound: 'sounds/principal_sound.mp3',
+        sound: 'sounds/principal_sound.m4a',
         type: 'mp3',
         volume: 0.2
       },
       quiz: {
-        sound: 'sounds/test.mp3',
+        sound: 'sounds/test_sound.m4a',
         type: 'mp3'
       },
       finaltest:{
-        sound: 'sounds/test.mp3',
+        sound: 'sounds/test_sound.m4a',
         type: 'mp3'
       }
     });
