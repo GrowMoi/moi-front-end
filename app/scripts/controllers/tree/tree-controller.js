3a5879288b7e3dd2a4aa701ece2ea855e1c10b2a
Darwin Gualotuña <daosgc@gmail.com>
2020-01-09 15:42:42 -0500

    Fix position of background app

--- app/scripts/controllers/tree/tree-controller.js
+++ app/scripts/controllers/tree/tree-controller.js
@@ -42,9 +42,9 @@
     var successAnswers = localStorage.getItem('successAnswers');
     var isShowingPassiveModal = false;
     var finishedAnimations = false;
-    var lastAchivement = dataInventory.achievements.filter(function(achievement) {
+    var lastAchivement = (dataInventory && dataInventory.achievements) ? dataInventory.achievements.filter(function(achievement) {
        return achievement.name === 'Contenidos aprendidos en cada neurona pública';
-    });
+    }) : [];
     treeModel.hideGrayTree = lastAchivement.length > 0;
     treeModel.frameOptions = {
       type: 'marco_arbol',
