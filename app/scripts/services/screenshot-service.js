31394ad50cbbad88828de5bca68b2f2994430922
Darwin Gualotuña <daosgc@gmail.com>
2020-12-02 08:05:30 -0500

    Update api to save tree screenshot and convert base64 to file the tree image W.I.P

--- app/scripts/services/screenshot-service.js
+++ app/scripts/services/screenshot-service.js
@@ -9,7 +9,8 @@
 
     var service = {
       getImage: getImage,
-      validBase64: validBase64
+      validBase64: validBase64,
+      getFileImage: getFileImage
     };
 
     function getImage(elm) {
@@ -22,6 +23,25 @@
       };
       html2canvas(elm, html2canvasOpts).then(function(canvas) { // jshint ignore:line
         deferred.resolve(canvas.toDataURL());
+      });
+      return deferred.promise;
+    }
+
+    function getFileImage(elm) {
+      var deferred = $q.defer();
+      var html2canvasOpts = {
+        backgroundColor: null,
+        allowTaint: false,
+        proxy: ENV.imagesProxy,
+        useCORS: true
+      };
+      html2canvas(elm, html2canvasOpts).then(function(canvas) { // jshint ignore:line
+        canvas.toBlob(function (blob) {
+          var timestamp = new Date().valueOf();
+          var fileName = 'screenshot_' + timestamp + '.png';
+          var file = new File([blob], fileName, { type: 'image/png' });
+          deferred.resolve(file);
+        }, 'image/jpeg');
       });
       return deferred.promise;
     }
